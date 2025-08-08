e77ecaf763b9e67992c229a77aa6bb85dc9dc7b7
netra212 <netra200021kcbdr@gmail.com>
2025-08-08 13:16:18 +0100

    Step 2 - Add skeleton routes for user

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CodeX
 Building an Course Selling Applications.
+
+## Create a course selling app.
+
+    - Initialize a new Node.js project. 
+    - Add Express, jswonwebtoken, mongoose to it as a dependency.
+    - Create index.js.
+    - Add route skeleton for user login, signup, purchase a course, sees all courses, sees the purchased courses course. 
+    - Add routes for admin login, adming signup, create a source, delete a source, add course content. 
+    - Add middlewares for user and admin auth. 
+    - Add a database (mongodb), use dotenv to store the database connection string. 
+    - Define the schema for User, Admin, Course, Purchase. 
+    - Complete the routes for user login, signup, purchase a course, see course (Extra points - Use express routing to better structure your routes).
+    - Create the frontend. 
--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-// 
-// 
-// 
 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// middleware for user. 
+function user_auth(req, res, next){
+
+}
+
+// middleware for admin. 
+function admin_auth(req, res, next){
+
+}
+
+app.post("/user/signup", function(req, res){
+    res.json({
+        message: "signup endpoint."
+    })
+})
+ 
+app.post("/user/signin", function(req, res){
+    res.json({
+        message: "signin endpoint."
+    })
+})
+
+app.get("/user/purchases", function(req, res){
+    res.json({
+        message: "purchase endpoint."
+    })
+})
+
+app.post("/course/purchase", function (req, res) {
+    // we would expect the user to pay the money.
+    res.json({
+        message: "user wanted to purchased course endpoint."
+    })
+})  
+ 
+app.get("/courses", function(req, res){
+    res.json({
+        message: "showing all courses endpoint."
+    })
+})
